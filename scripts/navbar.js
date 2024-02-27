fc84edcb9e96ba5ddc01bd21647f219a3fc678e0
Vendettyron <cocpalomobyronalejandro@gmail.com>
2024-02-26 18:40:54 -0600

    Mejoras generales
    Se agregaron imagnes al organigrama, animacion al organigrama, nueva iamgen en section quienes somos y ordenamiento de letras.
    
    Se optimizo el codigo de la navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,3 +1,8 @@
+// hacer que la pagina siempre cargue desde arriba
+window.onload = function() {
+    window.scrollTo(0, 0);
+  };
+
 //ABRIR Y CERRAR NAVBAR
 let btnToggleNavbar = document.getElementById('small_png2');
 let navbarAnimation =  document.getElementById('navbar');
@@ -12,34 +17,31 @@ btnToggleNavbar.addEventListener('click',toggleNavbar);
 
 var hamburger = document.querySelector(".hamburger");
 (function () {
-
     let navbar = document.querySelector('#navbar');
     let main = document.querySelector('main');
-    let navbarHeight = navbar.getBoundingClientRect().height;
-    let breakpoint = main.offsetTop - navbarHeight;
-    let windowPos;
+    let breakpoint = main.offsetTop - navbar.getBoundingClientRect().height;
     let isFixed = false;
-    
-    function updatePosition() {
-                windowPos =  window.scrollY;
-    }
-    
+
     function onScroll() {
-        updatePosition();
+        let windowPos = window.scrollY;
+        let navbarHeight = navbar.getBoundingClientRect().height;
+
         navbar.classList.remove('open');
         hamburger.classList.remove("is-active");
-        
+
         if (windowPos >= breakpoint && !isFixed) {
-                navbar.classList.add('navbar_fixed');
-                main.style.cssText = "margin-top:" + navbarHeight + 'px;';
-                isFixed = true;
-        }else if (windowPos < breakpoint && isFixed) {
-                navbar.classList.remove('navbar_fixed');
-                main.style.cssText = "margin-top:" + 0;
-                isFixed = false;
-        
+            navbar.classList.add('navbar_fixed');
+            main.style.marginTop = navbarHeight + 'px';
+            isFixed = true;
+        } else if (windowPos <= breakpoint && isFixed) {
+            navbar.classList.remove('navbar_fixed');
+            main.style.cssText = "margin-top:" + 0;
+            isFixed = false;
         }
     }
+
+    document.addEventListener('scroll', () => {
+        requestAnimationFrame(onScroll);
+    });
     
-    document.addEventListener('scroll', onScroll);    
-})()
+})();
